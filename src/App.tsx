086852bf02553eb3892333612aff318467407d85
castle <castle.h0326@gmail.com>
2024-11-23 02:04:34 +0900

    fix: 홈페이지 배포 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HeaderPage from './pages/HeaderPage'
 import HomePage from './pages/HomePage';
 import AuthPage from './pages/AuthPage';
+// import PopularPage from './pages/PopularPage';
+// import SearchPage from './pages/SearchPage';
+// import WishlistPage from './pages/WishlistPage';
 
 const router = createBrowserRouter([
   {
@@ -14,15 +17,44 @@ const router = createBrowserRouter([
     )
   },
   {
-    path: "/auth",
+    path: "/signin",
     element: (
       <div>
         <HeaderPage />
         <AuthPage />
       </div>
     )
-  }
-]);
+  },
+  // {
+  //   path: "/popular",
+  //   element: (
+  //     <div>
+  //       <HeaderPage />
+  //       <PopularPage />
+  //     </div>
+  //   )
+  // },
+  // {
+  //   path: "/search",
+  //   element: (
+  //     <div>
+  //       <HeaderPage />
+  //       <SearchPage />
+  //     </div>
+  //   )
+  // },
+  // {
+  //   path: "/wishlist",
+  //   element: (
+  //     <div>
+  //       <HeaderPage />
+  //       <WishlistPage />
+  //     </div>
+  //   )
+  // }
+], {
+  basename: '/wsd-assignment-02'
+});
 
 export default function App() {
   return <RouterProvider router={router} />;
