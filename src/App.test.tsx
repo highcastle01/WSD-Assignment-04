4c32db37d66f1a6ba156855d0fbfcae2daa65535
castle <castle.h0326@gmail.com>
2024-11-23 01:23:53 +0900

    fix: app.test 수정

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import { RouterProvider, createMemoryRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders app', () => {
+  const router = createMemoryRouter([
+    {
+      path: "/",
+      element: <App />,
+    },
+  ]);
+
+  render(<RouterProvider router={router} />);
 });
